8933d3e8f972805e7a95e33f5ccb5cb9ea214d6f
dbc2201 <divyanshb22@gmail.com>
2020-01-28 12:46:29 +0530

    add constructors for the Node class

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -39,5 +39,14 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
     private static class Node<E> {
         private E data;
         private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(E data) {
+            this.data = data;
+        }
     }
 }
