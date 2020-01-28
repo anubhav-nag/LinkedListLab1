0bbec36fe7e72d96a3baad12a11773a4084d4b0f
dbc2201 <divyanshb22@gmail.com>
2020-01-28 12:46:42 +0530

    make the constructors private

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -40,12 +40,12 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
         private E data;
         private Node<E> next;
 
-        public Node(E data, Node<E> next) {
+        private Node(E data, Node<E> next) {
             this.data = data;
             this.next = next;
         }
 
-        public Node(E data) {
+        private Node(E data) {
             this.data = data;
         }
     }
